61eb06268ece4883196143102ee68b69ec923e0c
Khaled Jemni <khaled.jemni@solid-apps.de>
2015-05-11 16:26:54 +0200

    Initial Commit

---
+++ iOS_Photoloop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_Photoloop
+//
+//  Created by Khaled Jemni on 11/05/15.
+//  Copyright (c) 2015 Khaled Jemni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS_Photoloop/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_Photoloop
+//
+//  Created by Khaled Jemni on 11/05/15.
+//  Copyright (c) 2015 Khaled Jemni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
