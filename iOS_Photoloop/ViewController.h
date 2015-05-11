67aa849f04ebc3fcd360943fab5e5ebc2d602ca3
Khaled Jemni <khaled.jemni@solid-apps.de>
2015-05-11 16:53:08 +0200

    Added the necessary actions and the timer for taking pictures

--- iOS_Photoloop/ViewController.h
+++ iOS_Photoloop/ViewController.h
@@ -7,9 +7,34 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+#import "AVCamPreviewView.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController< AVCaptureVideoDataOutputSampleBufferDelegate>
+{
+    bool shouldTakePicture;
+    AVCaptureStillImageOutput *stillImageOutput;
+    AVCaptureVideoDataOutput *videoDataOutput;
+    dispatch_queue_t videoDataOutputQueue;
+    AVCaptureDevice * videoDevice;
+    AVCaptureConnection *videoConnection;
+    AVCaptureDeviceInput *deviceInput;
+    NSMutableArray * pictures;
+}
 
+
+@property (strong, nonatomic) IBOutlet AVCamPreviewView *previewViewVideo;
+@property (weak, nonatomic) IBOutlet UIButton *takePic;
+@property (nonatomic, strong) id runtimeErrorHandlingObserver;
+@property (nonatomic, strong) AVCaptureSession *session;
+@property (nonatomic, strong) AVCaptureDeviceInput *videoDeviceInput;
+@property (nonatomic, strong) AVCaptureMovieFileOutput *movieFileOutput;
+@property (nonatomic, strong) dispatch_queue_t sessionQueue;
+@property (nonatomic) UIBackgroundTaskIdentifier backgroundRecordingID;
+@property (nonatomic) BOOL lockInterfaceRotation;
+
++ (AVCaptureDevice *)deviceWithMediaType:(NSString *)mediaType preferringPosition:(AVCaptureDevicePosition)position;
 
 @end
+
 
